1fde4c7e92f7519a69c3dd1e6e8858a89f45e2c5
Danton Nguyen <dantonnguyen88@gmail.com>
2019-04-25 13:08:45 -0700

    song list formatting

--- src/components/Song.js
+++ src/components/Song.js
@@ -5,8 +5,17 @@ class Song extends React.Component {
     return (
       <li className="song">
         <div className="song__details">
-          <div className="song__title">Song Title</div>
-          <div className="song__artist">Song Artist(s)</div>
+          <div className="song__title">
+            Song Title Song Title Song Title Song Title Song Title Song Title
+            Song Title Song Title{" "}
+          </div>
+          <div className="song__artist">
+            Song Artist(s) Song Artist(s) Song Artist(s) Song Artist(s) Song
+            Artist(s) Song Artist(s) Song Artist(s) Song Artist(s) Song
+            Artist(s) Song Artist(s) Song Artist(s) Song Artist(s) Song
+            Artist(s) Song Artist(s) Song Artist(s) Song Artist(s) Song
+            Artist(s){" "}
+          </div>
         </div>
         <button>+</button>
       </li>
